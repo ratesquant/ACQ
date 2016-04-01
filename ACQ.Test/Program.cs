874dc22606c596314c2d405b3519e284d59bdf19
Alex <rates.quant@gmail.com>
2016-03-31 23:25:52 -0400

    added test project, to simplify debugging

---
+++ ACQ.Test/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using ACQ.Math;
+
+namespace ACQ.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestInterpolation();
+        }
+
+        public static void TestInterpolation()
+        {
+            ACQ.Math.Interpolation.InterpolationFactory factory = new Math.Interpolation.InterpolationFactory();
+
+            double[] x = new double[]{1,2,3};
+            double[] y = new double[]{1,2,3};
+            double[,] f = new double[3, 3];
+
+            ACQ.Math.Interpolation.InterpolationFactory.GetInterpolator("Linear", x, y, true);
+
+            ACQ.Math.Interpolation.InterpolationFactory2D.GetInterpolator("Bilinear", x, y, f);
+ 
+        }
+    }
+}
