69651c049615852f21d009692c24a1e4bdae3f4a
Alex <rates.quant@gmail.com>
2016-04-06 22:23:59 -0400

    added Mersenne Twister

--- ACQ.Math/Random/MersenneTwister.cs
+++ ACQ.Math/Random/MersenneTwister.cs
@@ -7,52 +7,50 @@ namespace ACQ.Math.Random
 {
     #region MTNInfo
     /*
-   A C-program for MT19937, with initialization improved 2002/1/26.
-   Coded by Takuji Nishimura and Makoto Matsumoto.
+       A C-program for MT19937, with initialization improved 2002/1/26.
+       Coded by Takuji Nishimura and Makoto Matsumoto.
 
-   Before using, initialize the state by using init_genrand(seed)  
-   or init_by_array(init_key, key_length).
+       Before using, initialize the state by using init_genrand(seed)  
+       or init_by_array(init_key, key_length).
 
-   Copyright (C) 1997 - 2002, Makoto Matsumoto and Takuji Nishimura,
-   All rights reserved.                          
+       Copyright (C) 1997 - 2002, Makoto Matsumoto and Takuji Nishimura,
+       All rights reserved.                          
 
-   Redistribution and use in source and binary forms, with or without
-   modification, are permitted provided that the following conditions
-   are met:
+       Redistribution and use in source and binary forms, with or without
+       modification, are permitted provided that the following conditions
+       are met:
 
-     1. Redistributions of source code must retain the above copyright
-        notice, this list of conditions and the following disclaimer.
+         1. Redistributions of source code must retain the above copyright
+            notice, this list of conditions and the following disclaimer.
 
-     2. Redistributions in binary form must reproduce the above copyright
-        notice, this list of conditions and the following disclaimer in the
-        documentation and/or other materials provided with the distribution.
+         2. Redistributions in binary form must reproduce the above copyright
+            notice, this list of conditions and the following disclaimer in the
+            documentation and/or other materials provided with the distribution.
 
-     3. The names of its contributors may not be used to endorse or promote 
-        products derived from this software without specific prior written 
-        permission.
+         3. The names of its contributors may not be used to endorse or promote 
+            products derived from this software without specific prior written 
+            permission.
 
-   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-   A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
-   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+       THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+       "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+       LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+       A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+       CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+       EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+       PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+       PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+       LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+       NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+       SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
-   Any feedback is very welcome.
-   http://www.math.sci.hiroshima-u.ac.jp/~m-mat/MT/emt.html
-   email: m-mat @ math.sci.hiroshima-u.ac.jp (remove space)
+       Any feedback is very welcome.
+       http://www.math.sci.hiroshima-u.ac.jp/~m-mat/MT/emt.html
+       email: m-mat @ math.sci.hiroshima-u.ac.jp (remove space)
     */
     #endregion
     /// <summary>
-    /// Generates pseudo-random numbers using the Mersenne Twister algorithm (mt19937ar).
-    /// 
-    ///    
+    /// Generates pseudo-random numbers using the Mersenne Twister algorithm (code adapted from mt19937ar).   
     /// </summary>
     /// <remarks>
     /// See <a href="http://www.math.sci.hiroshima-u.ac.jp/~m-mat/MT/emt.html">
@@ -61,18 +59,18 @@ namespace ACQ.Math.Random
     /// </remarks>
     public class MersenneTwister : RandomBase
     {
-        /* Period parameters */
+        // Period parameters 
         private const int N = 624;
         private const int M = 397;
-        private const uint MatrixA = 0x9908b0df; /* constant vector a */
-        private const uint UpperMask = 0x80000000; /* most significant w-r bits */
-        private const uint LowerMask = 0x7fffffff; /* least significant r bits */
+        private const uint MatrixA = 0x9908b0df; // constant vector a
+        private const uint UpperMask = 0x80000000; // most significant w-r bits
+        private const uint LowerMask = 0x7fffffff; // least significant r bits 
 
-        /* Tempering parameters */
+        // Tempering parameters
         private const uint TemperingMaskB = 0x9d2c5680U;
         private const uint TemperingMaskC = 0xefc60000U;
 
-        private readonly uint[] m_mt = new uint[N]; /* the array for the state vector  */
+        private readonly uint[] m_mt = new uint[N]; // the array for the state vector  
         private int m_mti;
 
         private static readonly uint[] m_mag01 = { 0x0, MatrixA };
@@ -100,6 +98,7 @@ namespace ACQ.Math.Random
         {
             return genrand_real2();
         }
+
         public override int Next()
         {
             int value = genrand_int31();
