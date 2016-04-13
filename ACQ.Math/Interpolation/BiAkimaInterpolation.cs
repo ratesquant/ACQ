555ee342a032bab3981f1b26e3654b753b188c08
Alex <rates.quant@gmail.com>
2016-04-12 21:17:53 -0400

    added biAkima interpolation

---
+++ ACQ.Math/Interpolation/BiAkimaInterpolation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ACQ.Math.Interpolation
+{
+    public class BiAkimaInterpolation : BiInterpolation<AkimaInterpolation>
+    {
+        public BiAkimaInterpolation(double[] x1, double[] x2, double[,] y)
+            : base(x1, x2, y, true)
+        {
+        }
+        public BiAkimaInterpolation(double[] x1, double[] x2, double[,] y, bool copyData)
+            : base(x1, x2, y, copyData)
+        {
+        }
+    }
+}
