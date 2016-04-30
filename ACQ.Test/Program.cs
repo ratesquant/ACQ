3a2e10f97c714aebf5692711c72299052cace508
Alex <rates.quant@gmail.com>
2016-04-30 13:04:20 -0400

    added more test cases for linalg

--- ACQ.Test/Program.cs
+++ ACQ.Test/Program.cs
@@ -50,7 +50,8 @@ namespace ACQ.Test
         {  
             double eps = ACQ.Math.Const.epsilon;
             
-            for (int n = 3; n <= 128; n++) 
+            //test magic matrices
+            for (int n = 3; n <= 32; n++) 
             {
                 ACQ.Math.Linalg.Matrix M = ACQ.Math.Linalg.Matrix.CreateMagic(n);
 
@@ -84,9 +85,96 @@ namespace ACQ.Test
                 double rank = SV.Rank();
                 double cond = SV.Cond();
 
-
                 Console.WriteLine("n:{0}, trace:{1}, ev:{2}, lu:{3}, qr:{4}, sv:{5}", n, trace, d[d.Length - 1], lu_res, qr_res, sv_res); 
-            } 
+            }
+
+            //test random matrices
+            ACQ.Math.Random.IRandomGenerator rng = new ACQ.Math.Random.MersenneTwister();
+
+            for (int n = 1; n <= 8; n++)
+            {
+                ACQ.Math.Linalg.Matrix M = ACQ.Math.Linalg.Matrix.CreateRandom(n, n, rng);
+
+                ACQ.Math.Linalg.SvDecomposition SV = new ACQ.Math.Linalg.SvDecomposition(M);
+                ACQ.Math.Linalg.Matrix U = SV.GetU();
+                ACQ.Math.Linalg.Matrix S = SV.GetS();
+                ACQ.Math.Linalg.Matrix V = SV.GetV();
+                ACQ.Math.Linalg.Matrix R = U * S * V.Transpose() - M;
+                ACQ.Math.Linalg.Matrix pinv = SV.PseudoInverse();                
+                double pinv_res1 = (M * pinv * M - M).Norm1() / (n * eps);
+                double pinv_res2 = (pinv * M * pinv - pinv).Norm1() / (n * eps);                
+                double sv_res = R.Norm1() / (n * eps);
+                double rank = SV.Rank();
+                double cond = SV.Cond();
+
+                Console.WriteLine("n:{0}, rank:{1}, cond:{2}, sv:{3}, pinv:{4}, {5}", n, rank, cond, sv_res, pinv_res1, pinv_res2); 
+            }
+
+            //non square matrixes, m > n
+            for (int n = 1; n <= 32; n++)
+            {
+                for (int m = n; m <= 32; m++)
+                {
+                    ACQ.Math.Linalg.Matrix M = ACQ.Math.Linalg.Matrix.CreateRandom(m, n, rng);
+
+                    double err_scale = 1.0 / ( System.Math.Max(m, n) * eps );
+                    ACQ.Math.Linalg.SvDecomposition SV = new ACQ.Math.Linalg.SvDecomposition(M);
+                    ACQ.Math.Linalg.Matrix U = SV.GetU();
+                    ACQ.Math.Linalg.Matrix S = SV.GetS();
+                    ACQ.Math.Linalg.Matrix V = SV.GetV();
+                    ACQ.Math.Linalg.Matrix R = U * S * V.Transpose() - M;                    
+                    ACQ.Math.Linalg.Matrix pinv = SV.PseudoInverse();                    
+                    double pinv_res1 = (M * pinv * M - M).Norm1() * err_scale;
+                    double pinv_res2 = (pinv * M * pinv - pinv).Norm1() * err_scale;
+                    double sv_res = R.Norm1() * err_scale;
+                    double rank = SV.Rank();
+                    double cond = SV.Cond();
+
+                    Console.WriteLine("{0} x {1}, rank:{2:F4}, cond:{3:F4}, sv:{4:F4}, pinv:{5:F4}, {6:F4}", m, n, rank, cond, sv_res, pinv_res1, pinv_res2);
+                }
+            }
+
+            //linear systems: square with full rank
+            for (int n = 21; n <= 32; n++)
+            {
+                int m = 10;
+                ACQ.Math.Linalg.Matrix M = ACQ.Math.Linalg.Matrix.CreateRandom(n, n, rng);
+                ACQ.Math.Linalg.Matrix B = new Math.Linalg.Matrix(n, m);
+
+                for (int i = 0; i < M.Rows; i++)
+                {
+                    double sum = 0;
+                    for (int j = 0; j < M.Columns; j++)
+                    {
+                        sum += M[i, j];
+                    }
+
+                    for (int k = 0; k < B.Columns; k++)
+                    {
+                        B[i, k] = sum * (k + 1);                        
+                    }
+                }
+
+                double err_scale = 1.0 / (n * eps);
+
+                ACQ.Math.Linalg.SvDecomposition SV = new ACQ.Math.Linalg.SvDecomposition(M);
+                ACQ.Math.Linalg.Matrix X1 = SV.Solve(B);                                
+                ACQ.Math.Linalg.Matrix X2 = SV.PseudoInverse() * B;
+                double sv_res1 = (M * X1 - B).Norm1() * err_scale;
+                double sv_res2 = (M * X2 - B).Norm1() * err_scale;
+
+                ACQ.Math.Linalg.LuDecomposition LU = new ACQ.Math.Linalg.LuDecomposition(M);
+                X1 = LU.Solve(B);                
+                double lu_res1 = (M * X1 - B).Norm1() * err_scale;
+
+                ACQ.Math.Linalg.QrDecomposition QR = new ACQ.Math.Linalg.QrDecomposition(M);                
+                X1 = QR.Solve(B);
+                double qr_res1 = (M * X1 - B).Norm1() * err_scale;
+
+
+                Console.WriteLine("{0}, svd:{1:F4}, sv:{2:F4} lu:{3:F4} qr:{4:F4}", n, sv_res1, sv_res2, lu_res1, qr_res1);
+       
+            }
         }
         public static void TestInterpolation()
         {
