e661e8382ac132c074ccdd45e9b4e97d7e5d40ae
Alex <rates.quant@gmail.com>
2016-04-30 20:49:09 -0400

    added test cases for linear systems

--- ACQ.Test/Program.cs
+++ ACQ.Test/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
 using ACQ.Math;
 
@@ -11,6 +12,7 @@ namespace ACQ.Test
     {
         static void Main(string[] args)
         {
+            //TestThreadSafety();
             TestLinearAlgebra();
             //TestSort();
             //TestInterpolation();
@@ -46,6 +48,45 @@ namespace ACQ.Test
             }
  
         }
+
+        public static void TestThreadSafety()
+        {
+            ThreadPool.SetMaxThreads(16, 16);
+
+            for (int i = 0; i < 32*1024; i++)
+            {
+                Console.WriteLine("Adding {0}", i);
+                ThreadPool.QueueUserWorkItem(addin_function, i); //simulate excel calling our add-in function                                
+            }
+
+            //System.Threading.Thread thread = new System.Threading.Thread(ExecuteInForeground);
+            //thread.Start(10);                       
+            do
+            {
+                System.Threading.Thread.Sleep(500);                
+            } while (true);         
+        }
+
+        private static void addin_function(object a)
+        {
+            Console.WriteLine("Thread {0}: {1}", Thread.CurrentThread.ManagedThreadId, a);
+
+            //here we capture variable "a" and then compare it with "b" which is passed later
+            System.Threading.Thread thread = new System.Threading.Thread(
+                (object b) =>
+                {
+                    Console.WriteLine("Thread {0}: {1}, a:{2} b:{3}, {4}",
+                        Thread.CurrentThread.ManagedThreadId,
+                        Thread.CurrentThread.ThreadState, a, b, (int)a - (int)b);
+                    if (a != b)
+                    {
+                        throw new Exception("Different values detected");
+                    }                    
+                });
+            System.Threading.Thread.Sleep(20); //sleep to ensure that other threads can enter before we can finish
+            thread.Start(a);            
+        }
+
         public static void TestLinearAlgebra()
         {  
             double eps = ACQ.Math.Const.epsilon;
@@ -68,24 +109,24 @@ namespace ACQ.Test
                 ACQ.Math.Linalg.Matrix U = LU.GetU();
                 int[] p = LU.GetPivotIndex();
                 ACQ.Math.Linalg.Matrix R = L * U - M.Submatrix(p, 0, n - 1);
-                double lu_res = R.Norm1() / (n * eps);
+                double lu_res = R.Norm1();
 
                 ACQ.Math.Linalg.QrDecomposition QR = new ACQ.Math.Linalg.QrDecomposition(M);
                 ACQ.Math.Linalg.Matrix Q = QR.GetQ();
                 R = QR.GetR();
                 R = Q * R - M;
-                double qr_res = R.Norm1() / (n * eps);
+                double qr_res = R.Norm1();
 
                 ACQ.Math.Linalg.SvDecomposition SV = new ACQ.Math.Linalg.SvDecomposition(M);
                 U = SV.GetU();
                 ACQ.Math.Linalg.Matrix S = SV.GetS();
                 ACQ.Math.Linalg.Matrix V = SV.GetV();                
                 R = U * S * V.Transpose() - M;
-                double sv_res = R.Norm1() / (n * eps);
+                double sv_res = R.Norm1();
                 double rank = SV.Rank();
                 double cond = SV.Cond();
 
-                Console.WriteLine("n:{0}, trace:{1}, ev:{2}, lu:{3}, qr:{4}, sv:{5}", n, trace, d[d.Length - 1], lu_res, qr_res, sv_res); 
+                Console.WriteLine("n:{0,3} trace:{1:E4} ev:{2:E4} lu:{3:E4} qr:{4:E4} sv:{5:E4}", n, trace, d[d.Length - 1], lu_res, qr_res, sv_res); 
             }
 
             //test random matrices
@@ -101,13 +142,13 @@ namespace ACQ.Test
                 ACQ.Math.Linalg.Matrix V = SV.GetV();
                 ACQ.Math.Linalg.Matrix R = U * S * V.Transpose() - M;
                 ACQ.Math.Linalg.Matrix pinv = SV.PseudoInverse();                
-                double pinv_res1 = (M * pinv * M - M).Norm1() / (n * eps);
-                double pinv_res2 = (pinv * M * pinv - pinv).Norm1() / (n * eps);                
-                double sv_res = R.Norm1() / (n * eps);
+                double pinv_res1 = (M * pinv * M - M).Norm1();
+                double pinv_res2 = (pinv * M * pinv - pinv).Norm1();                
+                double sv_res = R.Norm1();
                 double rank = SV.Rank();
                 double cond = SV.Cond();
 
-                Console.WriteLine("n:{0}, rank:{1}, cond:{2}, sv:{3}, pinv:{4}, {5}", n, rank, cond, sv_res, pinv_res1, pinv_res2); 
+                Console.WriteLine("n:{0,3} rank:{1:E4} cond:{2:E4} sv:{3:E4} pinv:{4:E4} {5:E4}", n, rank, cond, sv_res, pinv_res1, pinv_res2); 
             }
 
             //non square matrixes, m > n
@@ -135,7 +176,7 @@ namespace ACQ.Test
             }
 
             //linear systems: square with full rank
-            for (int n = 21; n <= 32; n++)
+            for (int n = 1; n <= 32; n++)
             {
                 int m = 10;
                 ACQ.Math.Linalg.Matrix M = ACQ.Math.Linalg.Matrix.CreateRandom(n, n, rng);
@@ -171,9 +212,140 @@ namespace ACQ.Test
                 X1 = QR.Solve(B);
                 double qr_res1 = (M * X1 - B).Norm1() * err_scale;
 
+                Console.WriteLine("{0}, svd:{1:F4}, sv:{2:F4} lu:{3:F4} qr:{4:F4}", n, sv_res1, sv_res2, lu_res1, qr_res1);       
+            }
 
-                Console.WriteLine("{0}, svd:{1:F4}, sv:{2:F4} lu:{3:F4} qr:{4:F4}", n, sv_res1, sv_res2, lu_res1, qr_res1);
-       
+            //linear systems: symmetric with full rank
+            for (int n = 1; n <= 32; n++)
+            {
+                int m = 10;
+                ACQ.Math.Linalg.Matrix A = ACQ.Math.Linalg.Matrix.CreateRandom(n, n, rng);
+                ACQ.Math.Linalg.Matrix M = A * A.Transpose();
+                ACQ.Math.Linalg.Matrix B = new Math.Linalg.Matrix(n, m);
+
+                for (int i = 0; i < M.Rows; i++)
+                {
+                    double sum = 0;
+                    for (int j = 0; j < M.Columns; j++)
+                    {
+                        sum += M[i, j];
+                    }
+
+                    for (int k = 0; k < B.Columns; k++)
+                    {
+                        B[i, k] = sum * (k + 1);
+                    }
+                }
+
+                ACQ.Math.Linalg.SvDecomposition SV = new ACQ.Math.Linalg.SvDecomposition(M);
+                ACQ.Math.Linalg.Matrix X1 = SV.Solve(B);
+                ACQ.Math.Linalg.Matrix X2 = SV.PseudoInverse() * B;
+                double sv_res1 = (M * X1 - B).Norm1();
+                double sv_res2 = (M * X2 - B).Norm1();
+
+                ACQ.Math.Linalg.LuDecomposition LU = new ACQ.Math.Linalg.LuDecomposition(M);
+                X1 = LU.Solve(B);
+                double lu_res1 = (M * X1 - B).Norm1();
+
+                ACQ.Math.Linalg.QrDecomposition QR = new ACQ.Math.Linalg.QrDecomposition(M);
+                X1 = QR.Solve(B);
+                double qr_res1 = (M * X1 - B).Norm1();
+
+                ACQ.Math.Linalg.CholeskyDecomposition CD = new ACQ.Math.Linalg.CholeskyDecomposition(M);
+                X1 = CD.Solve(B);
+                double cd_res1 = (M * X1 - B).Norm1();
+
+                Console.WriteLine("{0,3}, sv:{1:E4} pinv:{2:E4} lu:{3:E4} qr:{4:E4}, cd:{5:E4}", n, sv_res1, sv_res2, lu_res1, qr_res1, cd_res1);
+            }
+
+            ACQ.Math.Random.IRandomGenerator rnorm = new ACQ.Math.Random.BoxMuller(rng);
+
+            //linear systems: overdetermined, full rank (least squares solution)
+            for (int n = 1; n <= 32; n++)
+            {
+                ACQ.Math.Linalg.Matrix M = ACQ.Math.Linalg.Matrix.CreateRandom(2 * n, n, rnorm);
+                ACQ.Math.Linalg.Matrix B = ACQ.Math.Linalg.Matrix.CreateRandom(2 * n, 1, rnorm);
+
+                for (int i = 0; i < M.Rows; i++)
+                {
+                    double sum = 0;
+                    for (int k = 0; k < M.Columns; k++)
+                    {
+                        sum += M[i, k];
+                    }
+                    B[i, 0] = sum + 0.1 * B[i, 0];
+                }
+
+                ACQ.Math.Linalg.SvDecomposition SV = new ACQ.Math.Linalg.SvDecomposition(M);
+                ACQ.Math.Linalg.Matrix Xsv = SV.Solve(B);
+                double sv_res1 = (M * Xsv - B).Norm1();
+                
+                ACQ.Math.Linalg.QrDecomposition QR = new ACQ.Math.Linalg.QrDecomposition(M);
+                ACQ.Math.Linalg.Matrix Xqr = QR.Solve(B);
+                double qr_res1 = (M * Xqr - B).Norm1();
+
+                double res = (Xsv - Xqr).Norm1();
+
+                Console.WriteLine("{0,3}, sv:{1:E4} qr:{2:E4} res:{3:E4}", n, sv_res1, qr_res1, res);
+            }
+
+            //linear systems: overdetermined but not full rank (least squares solution), 
+            //QR does not work in this case,
+            for (int n = 1; n <= 32; n++)
+            {
+                ACQ.Math.Linalg.Matrix M = ACQ.Math.Linalg.Matrix.CreateRandom(2 * n, n + 1, rnorm);
+                ACQ.Math.Linalg.Matrix B = ACQ.Math.Linalg.Matrix.CreateRandom(2 * n, 1, rnorm);
+
+                for (int i = 0; i < M.Rows; i++)
+                {
+                    M[i, n] = M[i, n - 1];//dublicate column
+                    double sum = 0;
+                    for (int k = 0; k < M.Columns; k++)
+                    {
+                        sum += M[i, k];
+                    }
+                    B[i, 0] = sum + 0.1 * B[i, 0];
+                }
+
+                //solution shold be approximatly all ones, last two values should be the same
+                //condition number is large (infinite actually, sometimes finite because of truncation errors) 
+                //rank = M.Columns - 1 (i.e. rank = n)
+                ACQ.Math.Linalg.SvDecomposition SV = new ACQ.Math.Linalg.SvDecomposition(M);
+                ACQ.Math.Linalg.Matrix Xsv = SV.Solve(B); 
+                double sv_res1 = (M * Xsv - B).Norm1();
+
+                ACQ.Math.Linalg.QrDecomposition QR = new ACQ.Math.Linalg.QrDecomposition(M);
+                double qr_res1 = Double.NaN;
+                double res = Double.NaN;
+                if (QR.IsFullRank)
+                {
+                    //occasionally QR thinks that matrix is full rank, 
+                    //but it is poorly conditioned so solution is wrong 
+                    ACQ.Math.Linalg.Matrix Xqr = QR.Solve(B);
+                    qr_res1 = (M * Xqr - B).Norm1();
+                    res = (Xsv - Xqr).Norm1();
+                }
+
+                Console.WriteLine("{0,3}, sv:{1:E4} qr:{2:E4} res:{3:E4}", n, sv_res1, qr_res1, res);
+            }
+
+            //linear systems: underdetermined (least norm solution)
+            for (int n = 1; n <= 32; n++)
+            {
+                ACQ.Math.Linalg.Matrix M = ACQ.Math.Linalg.Matrix.CreateRandom(n, 2 * n + 1, rnorm);
+                ACQ.Math.Linalg.Matrix B = ACQ.Math.Linalg.Matrix.CreateRandom(n, 1, rnorm);
+
+                for (int i = 0; i < M.Rows; i++)
+                {
+                    M[i, 2 * n] = M[i, 2 * n - 1];//dublicate column
+                }
+
+                ACQ.Math.Linalg.SvDecomposition SV = new ACQ.Math.Linalg.SvDecomposition(M.Transpose());
+                ACQ.Math.Linalg.Matrix Xsv = SV.Solve(B, true); //last two values should be the same
+                double sv_res1 = (M * Xsv - B).Norm1(); //this should be zero 
+                double x_norm = Xsv.Norm1();
+
+                Console.WriteLine("{0,3}, sv:{1:E4} xnorm:{2:E4}", n, sv_res1, x_norm);
             }
         }
         public static void TestInterpolation()
