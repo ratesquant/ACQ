571e53f6c680cdb99514229a91ef99d1948d63d8
Alex <rates.quant@gmail.com>
2016-05-02 21:36:05 -0400

    added few examples

--- ACQ.Excel/Examples.cs
+++ ACQ.Excel/Examples.cs
@@ -14,7 +14,32 @@ namespace ACQ.Excel
         public static double acq_sum(double a, double b)
         {
             return a + b;
+        }         
+
+        [ExcelFunction(Description = "Compute interpolated value", Category = AddInInfo.Category, IsThreadSafe = true)]
+        public static object acq_linear_interpolation(double xi, double[] x, double[] y, object bounds)
+        {
+            if (ExcelDnaUtil.IsInFunctionWizard())
+                return ExcelError.ExcelErrorRef;
+            else
+            {
+                ACQ.Math.Interpolation.InterpolationInterface interpolator = null;
+                try
+                {
+                    interpolator = new ACQ.Math.Interpolation.LinearInterpolation(x, y); //create linear interpolator 
+                    interpolator.Bounds = ExcelHelper.CheckValue(bounds, true);
+                }
+                catch (Exception ex)
+                {
+                    //LogDisplay.WriteLine("Error: " + ex.ToString());                    
+                }
+
+                if (interpolator != null)
+                    return ExcelHelper.CheckNan(interpolator.Eval(xi)); //interpolate
+                else
+                    return ExcelError.ExcelErrorNA;
+            }
         }
-         * */
+         */
     }
 }
