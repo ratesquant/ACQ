3bb520b61476dc21591e164f56fca7f41e2aaa9c
Alex <rates.quant@gmail.com>
2021-12-25 01:31:53 -0500

    added prime number checker

---
+++ ACQ.Math/Primes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ACQ.Math
+{
+    public class Primes
+    {
+        public static bool is_prime(int n)
+        {
+            bool result = true;
+            if (n == 0 || n == 1) //not primes
+            {
+                result = false;
+            }
+            else
+            {
+                for (int i = 2; i <= n / 2; ++i)
+                {
+                    if (n % i == 0)
+                    {
+                        result = false;
+                        break;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
