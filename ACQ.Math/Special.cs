9c288e9071f08351480f0f35d9bf2a5150ae1298
Alex <rates.quant@gmail.com>
2021-12-31 19:55:19 -0500

    added inverse normal CDF

--- ACQ.Math/Special.cs
+++ ACQ.Math/Special.cs
@@ -52,6 +52,8 @@ namespace ACQ.Math
         private const double big = 4.503599627370496e15;
         private const double biginv =  2.22044604925031308085e-16;
 
+        private const double s2pi = 2.50662827463100050242E0; //sqrt(2pi) 
+
         #endregion 
 
         #region polycoeffs
@@ -185,7 +187,73 @@ namespace ACQ.Math
             -0.6612093864662647,
             -0.2386191860831970
         };
-#endregion
+
+        private static readonly double[] ndtri_P0 = {
+            -5.99633501014107895267E1,
+            9.80010754185999661536E1,
+            -5.66762857469070293439E1,
+            1.39312609387279679503E1,
+            -1.23916583867381258016E0,
+        };
+        private static readonly double[] ndtri_Q0 = {
+            /* 1.00000000000000000000E0,*/
+            1.95448858338141759834E0,
+            4.67627912898881538453E0,
+            8.63602421390890590575E1,
+            -2.25462687854119370527E2,
+            2.00260212380060660359E2,
+            -8.20372256168333339912E1,
+            1.59056225126211695515E1,
+            -1.18331621121330003142E0,
+        };
+
+        private static readonly double[] ndtri_P1 = {
+            4.05544892305962419923E0,
+            3.15251094599893866154E1,
+            5.71628192246421288162E1,
+            4.40805073893200834700E1,
+            1.46849561928858024014E1,
+            2.18663306850790267539E0,
+            -1.40256079171354495875E-1,
+            -3.50424626827848203418E-2,
+            -8.57456785154685413611E-4,
+        };
+        private static readonly double[] ndtri_Q1 = {
+            /*  1.00000000000000000000E0,*/
+            1.57799883256466749731E1,
+            4.53907635128879210584E1,
+            4.13172038254672030440E1,
+            1.50425385692907503408E1,
+            2.50464946208309415979E0,
+            -1.42182922854787788574E-1,
+            -3.80806407691578277194E-2,
+            -9.33259480895457427372E-4,
+        };
+
+        private static readonly double[] ndtri_P2 = {
+            3.23774891776946035970E0,
+            6.91522889068984211695E0,
+            3.93881025292474443415E0,
+            1.33303460815807542389E0,
+            2.01485389549179081538E-1,
+            1.23716634817820021358E-2,
+            3.01581553508235416007E-4,
+            2.65806974686737550832E-6,
+            6.23974539184983293730E-9,
+        };
+        private static readonly double[] ndtri_Q2 = {
+            /*  1.00000000000000000000E0,*/
+            6.02427039364742014255E0,
+            3.67983563856160859403E0,
+            1.37702099489081330271E0,
+            2.16236993594496635890E-1,
+            1.34204006088543189037E-2,
+            3.28014464682127739104E-4,
+            2.89247864745380683936E-6,
+            6.79019408009981274425E-9,
+        };
+
+        #endregion
 
 
         /// <summary>
@@ -669,6 +737,11 @@ namespace ACQ.Math
         {
             return ndtr(x); 
         }
+
+        public static double InverseNormalCdf(double x)
+        {
+            return ndtri(x);
+        }
         public static double NormalPdf(double x)
         {
             return Special.SQT2PI_INV * System.Math.Exp(-0.5* x * x);
@@ -845,6 +918,56 @@ namespace ACQ.Math
             }
 
             return y;
+        }
+
+        /// <summary>
+        /// Inverse of Normal distribution function
+        /// </summary>
+        /// <param name="y0"></param>
+        /// <returns></returns>
+        public static  double ndtri(double y0)
+        {
+            double x, y, z, y2, x0, x1;
+            int code;
+            
+            if(y0 <= 0.0 )
+            {                
+                return Double.NegativeInfinity;
+            }
+            if(y0 >= 1.0 )
+            {
+                
+                return Double.PositiveInfinity;
+            }
+            
+            code = 1;
+            y = y0;
+            if (y > (1.0 - 0.13533528323661269189)) //0.135... = exp(-2) 
+            {
+                y = 1.0 - y;
+                code = 0;
+            }
+            
+            if (y > 0.13533528323661269189)
+            {
+                y = y - 0.5;
+                y2 = y * y;
+                x = y + y * (y2 * polevl(y2, ndtri_P0, 4) / p1evl(y2, ndtri_Q0, 8));
+                x = x * s2pi;
+                return (x);
+            }
+            
+            x = sqrt(-2.0 * log(y));
+            x0 = x - log(x) / x;
+            z = 1.0 / x;
+            if (x < 8.0) /* y > exp(-32) = 1.2664165549e-14 */
+                x1 = z * polevl(z, ndtri_P1, 8) / p1evl(z, ndtri_Q1, 8);
+            else
+                x1 = z * polevl(z, ndtri_P2, 8) / p1evl(z, ndtri_Q2, 8);
+            x = x0 - x1;
+            if (code != 0)
+                x = -x;
+            return (x);
         }
 
         /// <summary>
@@ -1267,6 +1390,10 @@ namespace ACQ.Math
         private static double fabs(double x)
         {
             return System.Math.Abs(x);
+        }
+        private static double sqrt(double x)
+        {
+            return System.Math.Sqrt(x);
         }
         private static double exp(double x)
         {
---
+++ ACQ.Math/Stats/BinomTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ACQ.Math.Stats
+{
+    /// <summary>
+    /// Binomial confidence intervals
+    /// https://en.wikipedia.org/wiki/Binomial_proportion_confidence_interval
+    /// </summary>
+    public class BinomTest
+    {
+        /// <summary>
+        /// Confidence interval for the binomial probability.
+        /// </summary>
+        /// <param name="x">Vector of number of successes in the binomial experiment</param>
+        /// <param name="n">Vector of number of independent trials in the binomial experiment.</param>
+        /// <param name="conf_level"></param>
+        /// <returns></returns>
+        public static Tuple<double, double> ConfidenceInterval(int x, int n, double conf_level = 0.95)
+        {
+            double p = (double) x / n;
+            double alpha = 1 - conf_level;
+            double z = ACQ.Math.Special.InverseNormalCdf(1 - 0.5 * alpha);
+            double z2 = z * z;
+
+            double p1 = p + 0.5 * z2 / n;
+            double p2 = z * System.Math.Sqrt((p * (1 - p) + 0.25 * z2 / n) / n);
+            double p3 = 1 + z2 / n;
+            double lcl = (p1 - p2) / p3;
+            double ucl = (p1 + p2) / p3;
+
+            return new Tuple<double, double>(lcl, ucl);
+        }
+    }
+}
